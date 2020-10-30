f01fcf77089b3b90376182adc7a32aa56f5b3522
Sungmin Yang <kimchhickey@gmail.com>
2020-10-30 17:26:09 +0900

    2018 d2

--- src/advent_of_code/y2018/d2.clj
+++ src/advent_of_code/y2018/d2.clj
@@ -6,23 +6,65 @@
 (def input
   (line-seq (io/reader (io/resource "y2018/d2.input"))))
 
-(defn n-times? [n]
-  (fn [coll]
-    (not (empty? (filter #(= n (second %)) coll)))))
 
-(def two-times? (n-times? 2))
-(def three-times? (n-times? 3))
+(def not-nil? (complement nil?))
 
-(defn inc-true [acc val]
-  (if (true? val) (inc acc) acc))
-
-(defn inc-true-vec [acc val]
-  [(inc-true (first acc) (first val))
-   (inc-true (second acc) (second val))])
-
-; p1
+;; part 1
 (->> input
-     (map #(frequencies (seq %)))
-     (map (juxt two-times? three-times?))
-     (reduce inc-true-vec [0 0])
-     (reduce *))
+     (map frequencies)
+     (map vals)
+     (map #(into #{} %))
+     (map #(map #{2 3} %))
+     (map #(filter not-nil? %))
+     flatten
+     frequencies
+     vals
+     (apply *))
+
+;; part 2
+(defn diff [s1 s2]
+  (reduce (fn [acc itm]
+            (let [target (:target acc)
+                  keys (:keys acc)]
+              (if (= itm (first target))
+                (update acc :target rest)
+                (-> acc
+                    (assoc :keys (conj keys itm))
+                    (update :count inc)
+                    (update :target rest)))))
+          {:target s2
+           :count 0
+           :keys []}
+          s1))
+
+(def ans-set (reduce (fn [acc itm1]
+                       (let [find-ans
+                             (reduce (fn [acc itm2]
+                                       (if (= 1 (:count (count-diff itm1 itm2)))
+                                         (reduced #{itm1 itm2})
+                                         []))
+                                     []
+                                     acc)]
+                         (if (set? find-ans)
+                           (reduced find-ans)
+                           acc)))
+                     input
+                     input))
+(def diff-char
+  (first (:keys (diff (first ans-set)
+                     (second ans-set)))))
+
+(->> (first ans-set)
+     (filter #(not= diff-char %))
+     (apply str))
+
+(comment
+  ;; p1
+  (def one (first input))
+  (filter not-nil? (map #{2 3} (into #{} (vals (frequencies one)))))
+
+  ;; p2
+  (def two (second input))
+  (count-diff one two)
+  )
+
