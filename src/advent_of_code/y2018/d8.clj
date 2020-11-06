24391018fdfe2d8273143e9acec9d66d2968cbc0
Sungmin Yang <kimchhickey@gmail.com>
2020-11-06 10:16:27 +0900

    y2018 d8 ready

---
+++ src/advent_of_code/y2018/d8.clj
@@ -0,0 +1,8 @@
+(ns advent_of_code.y2018.d8
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [advent_of_code.util :as u]))
+
+(def input
+  (->> "y2018/d8.test.input"
+       (u/read-input)))
